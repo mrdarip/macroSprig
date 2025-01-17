e1da20b2ae1c6f512c270f1b4833c61d22d88134
mrdarip <darioelas2005@gmail.com>
2025-01-17 17:48:43 +0100

    Untrack files in .gitignore

--- build/libraries/Adafruit_GFX_Library/glcdfont.c.d
+++
@@ -1,2 +0,0 @@
-/home/mrdarip/git/macroSprig/build/libraries/Adafruit_GFX_Library/glcdfont.c.o: \
- /home/mrdarip/Arduino/libraries/Adafruit_GFX_Library/glcdfont.c
--- build/libraries/ESP8266SdFat/FsLib/FsNew.cpp.d
+++
@@ -1,3 +0,0 @@
-/home/mrdarip/git/macroSprig/build/libraries/ESP8266SdFat/FsLib/FsNew.cpp.o: \
- /home/mrdarip/.arduino15/packages/rp2040/hardware/rp2040/4.4.1/libraries/ESP8266SdFat/src/FsLib/FsNew.cpp \
- /home/mrdarip/.arduino15/packages/rp2040/hardware/rp2040/4.4.1/libraries/ESP8266SdFat/src/FsLib/FsNew.h
